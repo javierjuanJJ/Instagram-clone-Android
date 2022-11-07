acfc1c368fac8deb0a2b3fd86d0cb91023e6d110
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-07 12:30:38 +0100

    Universal Image Loader Config (Part 14) - [Build an Instagram Clone]

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/EditProfileFragment.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/EditProfileFragment.java
@@ -1,23 +1,45 @@
 package whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+
 class EditProfileFragment extends Fragment {
 
     private static final String TAG = EditProfileFragment.class.getSimpleName();
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
 
+        mProfilePhoto = view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
+
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2016/08/ac-lloyd.jpg?itok=bb72IeLf";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 
 }
