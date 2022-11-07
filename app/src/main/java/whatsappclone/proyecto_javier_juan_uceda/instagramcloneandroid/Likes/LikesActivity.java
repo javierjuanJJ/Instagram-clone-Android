19c77c502fc9fcf339acd7b84ac84ad1d929b898
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-07 13:03:23 +0100

    Register Layout (Part 18) - [Build an Instagram Clone]

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Likes/LikesActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Likes/LikesActivity.java
@@ -18,7 +18,7 @@ public class LikesActivity extends ParentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         setUI();
 
