c5c9629c18029cf5c7d1940b5fe35870603484f1
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-07 11:33:35 +0100

    SectionsPagerAdapter Tabs (Part 6) - [Build an Instagram Clone]

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Home/HomeActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Home/HomeActivity.java
@@ -3,7 +3,10 @@ package whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.Home;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.ParentActivity;
 import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.R;
@@ -29,5 +32,22 @@ public class HomeActivity extends ParentActivity {
         bottomNavigationView = findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
         m1(TAG, ACTIVITY_NUM);
+        setupViewPager();
+    }
+
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessagesFragment()); //index 2
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.mipmap.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.mipmap.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.mipmap.ic_arrow);
     }
 }
