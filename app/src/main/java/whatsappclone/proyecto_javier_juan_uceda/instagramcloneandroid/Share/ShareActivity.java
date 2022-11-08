f75ef931e72e4413f7bb4710637bcfcd88a1126a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-08 12:45:45 +0100

    Verifying Permissions for Sharing (Part 41) - [Build an Instagram Clone]

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Share/ShareActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Share/ShareActivity.java
@@ -1,19 +1,24 @@
 package whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.Share;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.ParentActivity;
 import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.R;
 import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.Utils.BottomNavigationViewHelper;
+import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.Utils.Permissions;
 
 public class ShareActivity extends ParentActivity {
 
     private static final String TAG = ShareActivity.class.getSimpleName();
     private BottomNavigationView bottomNavigationView;
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,60 @@ public class ShareActivity extends ParentActivity {
 
     private void setUI() {
         Log.d(TAG, "onCreate: starting.");
-        bottomNavigationView = findViewById(R.id.bottomNavViewBar);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
-        m1(TAG, ACTIVITY_NUM);
+//        bottomNavigationView = findViewById(R.id.bottomNavViewBar);
+//        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+//        m1(TAG, ACTIVITY_NUM);
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+
+        }else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+    }
+
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    /**
+     * Check an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions){
+        Log.d(TAG, "checkPermissionsArray: checking permissions array.");
+
+        for(int i = 0; i< permissions.length; i++){
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check a single permission is it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permission was granted for: " + permission);
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Utils/Permissions.java
@@ -0,0 +1,12 @@
+package whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.Utils;
+
+import android.Manifest;
+
+public class Permissions {
+
+   public static final String[] PERMISSIONS = {
+           Manifest.permission.WRITE_EXTERNAL_STORAGE,
+           Manifest.permission.READ_EXTERNAL_STORAGE,
+           Manifest.permission.CAMERA
+   };
+}
