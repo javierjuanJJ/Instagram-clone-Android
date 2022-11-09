cbdb701ec71e56c8cba0389e00f4de9575456cdf
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-09 10:34:08 +0100

    GalleryFragment Layout (Part 45) - [Build an Instagram Clone]

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Share/GalleryFragment.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/Share/GalleryFragment.java
@@ -9,6 +9,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid.R;
 
@@ -16,11 +21,43 @@ public class GalleryFragment extends Fragment {
 
     private static final String TAG = GalleryFragment.class.getSimpleName();
 
+    //widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner directorySpinner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         Log.d(TAG, "onCreateView: started.");
+
+        galleryImage = (ImageView) view.findViewById(R.id.galleryImageView);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+
+        ImageView shareClose = (ImageView) view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the gallery fragment.");
+                getActivity().finish();
+            }
+        });
+
+
+        TextView nextScreen = (TextView) view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to the final share screen.");
+
+
+            }
+        });
 
         return view;
     }
