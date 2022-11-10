5b52c8906e28b009db58590a7f31d937cc29e4ef
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-10 11:59:14 +0100

    part80 - Inserting Comments

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/ViewCommentsFragment.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/instagramcloneandroid/ViewCommentsFragment.java
@@ -1,5 +1,6 @@
 package whatsappclone.proyecto_javier_juan_uceda.instagramcloneandroid;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -9,10 +10,12 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -63,6 +66,12 @@ public class ViewCommentsFragment extends Fragment {
     //vars
     private Photo mPhoto;
     private ArrayList<Comment> mComments;
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+
 
     @Nullable
     @Override
@@ -75,6 +84,7 @@ public class ViewCommentsFragment extends Fragment {
         mListView = (ListView) view.findViewById(R.id.listView);
         mComments = new ArrayList<>();
 
+        setupFirebaseAuth();
 
         try{
             mPhoto = getPhotoFromBundle();
@@ -93,6 +103,23 @@ public class ViewCommentsFragment extends Fragment {
         CommentListAdapter adapter = new CommentListAdapter(getActivity(),
                 R.layout.layout_comments, mComments);
         mListView.setAdapter(adapter);
+
+        mCheckMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!mComment.getText().toString().equals("")){
+                    Log.d(TAG, "onClick: attempting to submit new comment.");
+                    addNewComment(mComment.getText().toString());
+
+                    mComment.setText("");
+                    closeKeyboard();
+                }else{
+                    Toast.makeText(getActivity(), "you can't post a blank comment", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return view;
     }
 
@@ -108,6 +135,97 @@ public class ViewCommentsFragment extends Fragment {
             return bundle.getParcelable(getString(R.string.photo));
         }else{
             return null;
+        }
+    }
+
+    private void closeKeyboard(){
+        View view = getActivity().getCurrentFocus();
+        if(view != null){
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+
+    private void addNewComment(String newComment){
+        Log.d(TAG, "addNewComment: adding new comment: " + newComment);
+
+        String commentID = myRef.push().getKey();
+
+        Comment comment = new Comment();
+        comment.setComment(newComment);
+        comment.setDate_created(getTimestamp());
+        comment.setUser_id(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        //insert into photos node
+        myRef.child(getString(R.string.dbname_photos))
+                .child(mPhoto.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .child(commentID)
+                .setValue(comment);
+
+        //insert into user_photos node
+        myRef.child(getString(R.string.dbname_user_photos))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPhoto.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .child(commentID)
+                .setValue(comment);
+
+    }
+
+    private String getTimestamp(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.CANADA);
+        sdf.setTimeZone(TimeZone.getTimeZone("Canada/Pacific"));
+        return sdf.format(new Date());
+    }
+
+    /*
+    ------------------------------------ Firebase ---------------------------------------------
+     */
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 
